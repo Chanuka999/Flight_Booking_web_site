fe70fa5cd45ea715786b90b8a3c909e247188d21
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-04 01:24:34 +0530

    create file Navbar

---
+++ fly/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navBar flex">
+      <div className="navBarOne flex">
+        <div>
+          <SiConsul />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
