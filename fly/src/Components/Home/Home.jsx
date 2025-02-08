b7c45095f38ceb8eb195287d110f1f7221226554
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-08 19:57:42 +0530

    update home file

--- fly/src/Components/Home/Home.jsx
+++ fly/src/Components/Home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import video from "../../assets/video.mp4";
+import aeroplane from "../../assets/aeroplane.webp";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home flex container">
+      <div className="mainText">
+        <h1>Create Ever-lasting Memories With us</h1>
+      </div>
+
+      <div className="homeImages flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop className="video"></video>
+        </div>
+        <img src={aeroplane} className="plane" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
