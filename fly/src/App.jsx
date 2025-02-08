2567447d7ccfdea9f3a18d9bc7c8c507825f213c
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-08 11:13:25 +0530

    import scss fime location

--- fly/src/App.jsx
+++ fly/src/App.jsx
@@ -1,26 +1,20 @@
 import React from "react";
+import "./main.scss";
+
 import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import Search from "./Components/search/Search";
-import Support from "./Components/Support/Support";
-import Info from "./Components/Info/Info";
-import Lounge from "./Components/Lounge/Lounge";
-import Travelers from "./Components/Travelers/Travelers";
-import Subscribers from "./Components/Subscribers/Subscribers";
-import Footer from "./Components/Footer/Footer";
+//import Home from "./Components/Home/Home";
+//import Search from "./Components/search/Search";
+//import Support from "./Components/Support/Support";
+//import Info from "./Components/Info/Info";
+//import Lounge from "./Components/Lounge/Lounge";
+//import Travelers from "./Components/Travelers/Travelers";
+//import Subscribers from "./Components/Subscribers/Subscribers";
+//import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Home />
-      <Search />
-      <Support />
-      <Info />
-      <Lounge />
-      <Travelers />
-      <Subscribers />
-      <Footer />
     </div>
   );
 };
