b88be3681722e53f2ca0bfa15c48a8120d29bb1c
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-14 14:25:49 +0530

    add to lounge page

--- fly/src/App.jsx
+++ fly/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./Components/Home/Home";
 import Search from "./Components/search/Search";
 import Support from "./Components/Support/Support";
 import Info from "./Components/Info/Info";
-//import Lounge from "./Components/Lounge/Lounge";
+import Lounge from "./Components/Lounge/Lounge";
 //import Travelers from "./Components/Travelers/Travelers";
 //import Subscribers from "./Components/Subscribers/Subscribers";
 //import Footer from "./Components/Footer/Footer";
@@ -19,6 +19,7 @@ const App = () => {
       <Search />
       <Support />
       <Info />
+      <Lounge />
     </div>
   );
 };
