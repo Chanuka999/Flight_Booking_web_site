14173fa561920b36f07d9dce3fe9feffdcb8659e
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-14 18:49:14 +0530

    add travelers page

--- fly/src/App.jsx
+++ fly/src/App.jsx
@@ -7,7 +7,7 @@ import Search from "./Components/search/Search";
 import Support from "./Components/Support/Support";
 import Info from "./Components/Info/Info";
 import Lounge from "./Components/Lounge/Lounge";
-//import Travelers from "./Components/Travelers/Travelers";
+import Travelers from "./Components/Travelers/Travelers";
 //import Subscribers from "./Components/Subscribers/Subscribers";
 //import Footer from "./Components/Footer/Footer";
 
@@ -20,6 +20,7 @@ const App = () => {
       <Support />
       <Info />
       <Lounge />
+      <Travelers />
     </div>
   );
 };
