e2e069c90d466691c93435ce4c3baf1f1e53bfa6
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-15 14:40:48 +0530

    update code

--- fly/src/Components/Travelers/Travelers.jsx
+++ fly/src/Components/Travelers/Travelers.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 import paris from "../../assets/paris.jfif";
+import dubai from "../../assets/dubai.jfif";
+import newyork from "../../assets/newyork.jpg";
+import london from "../../assets/london.jpg";
+
 import traveler1 from "../../assets/traveler1.jpeg";
+import traveler2 from "../../assets/traveler2.jfif";
+import traveler3 from "../../assets/traveler3.jpg";
+import traveler4 from "../../assets/traveler4.jpg";
+
+const traverlers = [
+  {
+    id: 1,
+    destinationImage: paris,
+    travelerImage: traveler1,
+    travelerName: "jack",
+    socialLink: "@jackfred",
+  },
+
+  {
+    id: 2,
+    destinationImage: dubai,
+    travelerImage: traveler2,
+    travelerName: "joe",
+    socialLink: "@joensd",
+  },
+  {
+    id: 3,
+    destinationImage: newyork,
+    travelerImage: traveler3,
+    travelerName: "smith",
+    socialLink: "@maxsmith",
+  },
+  {
+    id: 4,
+    destinationImage: london,
+    travelerImage: traveler4,
+    travelerName: "john",
+    socialLink: "@johnferd",
+  },
+];
 
 const Travelers = () => {
   return (
@@ -10,18 +49,30 @@ const Travelers = () => {
         <h3>Top traverlers of this month</h3>
 
         <div className="travelersContainer grid">
-          <div className="singleTraveler">
-            <img src={paris} className="destinationImage" />
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src={traveler1} className="travelerImage" />
-              </div>
-              <div className="travelerName">
-                <span>Jack</span>
-                <p>@jackfred</p>
-              </div>
-            </div>
-          </div>
+          {traverlers.map(
+            ({
+              id,
+              destinationImage,
+              travelerImage,
+              travelerName,
+              socialLink,
+            }) => {
+              return (
+                <div key={id} className="singleTraveler">
+                  <img src={destinationImage} className="destinationImage" />
+                  <div className="travelerDetails">
+                    <div className="travelerPicture">
+                      <img src={travelerImage} className="travelerImage" />
+                    </div>
+                    <div className="travelerName">
+                      <span>{travelerName}</span>
+                      <p>{socialLink}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
     </div>
