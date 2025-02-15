fa55b98b183bc699d48a9e437e845fb9689df252
Chanuka999 <chanukaranditha99@gmail.com>
2025-02-15 17:49:18 +0530

    remove unnesary code

--- fly/src/Components/Subscribers/Subscribers.jsx
+++ fly/src/Components/Subscribers/Subscribers.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+//import React from "react";
 
 const Subscribers = () => {
-  return <div>Subscribers</div>;
+  return (
+    <div className="subscribe section">
+      <div className="sectionContainer container">
+        <h3>Subscribe Newsletters & get latest News</h3>
+        <div className="inputDiv flex">
+          <input type="text" placeholder="Enter your email address" />
+          <button className="btn">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Subscribers;
